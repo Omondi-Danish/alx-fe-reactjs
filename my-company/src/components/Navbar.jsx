50059a19550265dd33a242351a98988f7006d40c
Omondi Danish <omondidanish77@gmail.com>
2025-11-09 13:44:41 +0300

    update

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,11 +2,26 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ backgroundColor: "navy", padding: "10px" }}>
-      <Link to="/" style={{ color: "white", margin: "10px" }}>Home</Link>
-      <Link to="/about" style={{ color: "white", margin: "10px" }}>About</Link>
-      <Link to="/services" style={{ color: "white", margin: "10px" }}>Services</Link>
-      <Link to="/contact" style={{ color: "white", margin: "10px" }}>Contact</Link>
+    <nav
+      style={{
+        backgroundColor: "navy",
+        padding: "15px",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <Link to="/" style={{ color: "white", margin: "0 15px" }}>
+        Home
+      </Link>
+      <Link to="/about" style={{ color: "white", margin: "0 15px" }}>
+        About
+      </Link>
+      <Link to="/services" style={{ color: "white", margin: "0 15px" }}>
+        Services
+      </Link>
+      <Link to="/contact" style={{ color: "white", margin: "0 15px" }}>
+        Contact
+      </Link>
     </nav>
   );
 }
