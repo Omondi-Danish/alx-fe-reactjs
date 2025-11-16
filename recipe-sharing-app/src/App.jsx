398e7afa9164414e0b919a74817a38e8478c5522
Omondi Danish <omondidanish77@gmail.com>
2025-11-16 18:27:26 +0300

    Implement Router, path, react-router-dom, Route, Routes

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,12 +1,25 @@
+import { Routes, Route } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails';
 
 function App() {
   return (
     <div style={{ padding: '2rem' }}>
       <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
+      {/* Define routes here */}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <AddRecipeForm />
+              <RecipeList />
+            </>
+          }
+        />
+        <Route path="/recipes/:recipeId" element={<RecipeDetails />} />
+      </Routes>
     </div>
   );
 }
